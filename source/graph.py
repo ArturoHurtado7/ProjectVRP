c2ed5874c0103b6c20dfbff6c9a450ebf794ce14
Arturo Hurtado <ja.hurtado905@uniandes.edu.co>
2022-04-28 23:22:43 -0500

    connect initial solution

--- source/graph.py
+++ source/graph.py
@@ -7,22 +7,9 @@ class Graph():
     def __init__(self, nodes, capacity) -> None:
         """ 
         initializes a graph object
-        - depot is the first node (0)
         """
         self.capacity = capacity
         self.nodes = dict(nodes)
-        #self.edges = self.get_edges()
-
-
-    def get_edges(self):
-        """
-        Get edges of the graph
-        """
-        linehauls = self.linehaul_nodes()
-        backhauls = self.backhaul_nodes()
-        edges_linehaul = self.nodes_edges(linehauls)
-        edges_backhaul = self.nodes_edges(backhauls)
-        return edges_linehaul + edges_backhaul
 
 
     def nodes_edges(self, nodes):
@@ -30,15 +17,16 @@ class Graph():
         Get edges of the nodes
         """
         edges = []
-        for x in nodes.keys():
-            for y in nodes.keys():
-                if x > y:
-                    distance = self.euclidean_distance(x, y)
-                    edges.append([x, y, distance])
+        keys = list(nodes.keys())
+        for i, x in enumerate(keys):
+            for j in range(i+1, len(keys)):
+                y = keys[j]
+                distance = self.euclidean_distance(x, y)
+                edges.append([x, y, distance])
         return edges
 
 
-    def type_nodes(self, type_node):
+    def type_nodes(self, type_node: str) -> dict:
         """
         Get nodes of a specific type
         """
@@ -47,9 +35,9 @@ class Graph():
             if value['type_node'] == type_node:
                 nodes[key] = value
         return nodes
-    
 
-    def nodes_route(self, route) -> list:
+
+    def nodes_route(self, route: list) -> list:
         """
         Return nodes from route
         """
@@ -73,7 +61,7 @@ class Graph():
         return self.type_nodes('B')
 
 
-    def euclidean_distance(self, x, y) -> float:
+    def euclidean_distance(self, x: int, y: int) -> float:
         """
         returns the euclidean distance between two nodes
         """
@@ -84,7 +72,7 @@ class Graph():
         return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
 
-    def route_distance(self, route) -> float:
+    def route_distance(self, route: list) -> float:
         """
         returns the distance of a route
         """
@@ -99,8 +87,8 @@ class Graph():
         returns the demand of a route
         """
         demand = 0
-        for i in range(1, len(route)):
-            demand += self.nodes[route[i]]['demand']
+        for r in route:
+            demand += self.nodes[r]['demand']
         return demand
 
 
@@ -109,8 +97,8 @@ class Graph():
         returns the distance and angle between depot or initial_node and selected node
         """
         # get coordinates
-        x1, y1 = self.nodes[initial_node]['coordinates'] # Depot
-        x2, y2 = self.nodes[node]['coordinates'] # Selected node
+        x1, y1 = self.nodes[initial_node]['coordinates']
+        x2, y2 = self.nodes[node]['coordinates']
         # calculate distance and angle
         distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
         angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
@@ -157,22 +145,81 @@ class Graph():
         """
         Initial solution with a greedy algorithm
         """
-        linehaul_nodes = self.linehaul_nodes()
         backhaul_nodes = self.backhaul_nodes()
-        linehauls = self.sweep_algorithm(linehaul_nodes)
+        linehaul_nodes = self.linehaul_nodes()
         backhauls = self.sweep_algorithm(backhaul_nodes)
+        linehauls = self.sweep_algorithm(linehaul_nodes)
         while len(linehauls) < len(backhauls):
-            linehauls = [[0, linehauls[-1].pop()]] + linehauls
-        routes = linehauls + backhauls
-        print(routes)
+            i = -1
+            while len(linehauls[i]) <= 1: i -= 1
+            linehauls = [[0, linehauls[i].pop()]] + linehauls
+        end_linehauls = [linehaul[-1] for linehaul in linehauls]
+        end_backhauls = [backhaul[-1] for backhaul in backhauls]
+        distance_matrix = self.distance_end_nodes(end_linehauls, end_backhauls)
+        assignment = self.optimal_assignment(distance_matrix)
+        routes = []
+        for u, v in assignment:
+            backhauls[v].reverse()
+            routes.append(linehauls[u] + backhauls[v])
+
         # print results
+        print('routes:', routes)
         print('capacity:', self.capacity)
         print('clusters: ', routes)
-        distances = [self.route_distance(route) for route in routes]
-        print('distances: ', distances)
-        print('total distance: ', sum(distances))
+        print('distances: ', [self.route_distance(route) for route in routes])
+        print('total distance: ', sum([self.route_distance(route) for route in routes]))
         print('demands', [self.route_demand(route) for route in routes])
         self.plot_graph(routes)
+
+        return routes
+
+
+    def distance_end_nodes(self, end_linehauls, end_backhauls):
+        """
+        Calculate the distance between the end nodes of the linehauls and backhauls
+        """
+        distance_matrix = []
+        for i in end_linehauls:
+            distance_rows = []
+            for j in end_backhauls:
+                distance_rows.append(self.euclidean_distance(i, j))
+            distance_matrix.append(distance_rows)
+        return distance_matrix
+    
+
+    def optimal_assignment(self, distance_matrix):
+        recursive, _ = self.recursive_assignment(distance_matrix, 0, [])
+        assignment = []
+        for row in recursive: 
+            assignment += row
+        assignment = sorted(assignment, key=lambda x: x[0])
+        optimal, i = [], 0
+        while assignment[i][0] == assignment[0][0]:
+            if (assignment[i][1][0] not in [o[0] for o in optimal] and
+                assignment[i][1][1] not in [o[1] for o in optimal]):
+                optimal.append(assignment[i][1])
+            i += 1
+        return optimal
+
+
+    def recursive_assignment(self, distance_matrix: list, accumulated_distance: int, route: list) -> tuple:
+        """
+        Get the optimal assignment by recursively adding the closest node to the route
+        """
+        answer = []
+        if len(distance_matrix) == 1:
+            return accumulated_distance + distance_matrix[0][0], route
+        for i, rows in enumerate(distance_matrix):
+            for j, distance in enumerate(rows):
+                d_matrix = [[i for x, i in enumerate(row) if x!= j] for y, row in enumerate(distance_matrix) if y!= i]
+                d_distance = accumulated_distance + distance
+                d_route = route if route else [i, j]
+                r_matrix, r_route = self.recursive_assignment(d_matrix, d_distance, d_route)
+                if r_route:
+                    answer.append([r_matrix, r_route])
+                else:
+                    answer.append(r_matrix)
+        return answer, []
 
 
     def sweep_algorithm(self, nodes):
--- source/main.py
+++ source/main.py
@@ -6,12 +6,10 @@ from graph import Graph
 
 def main():
     utility = Utility()
-    input = utility.read_input('./data/eil22_50_2.vrp')
+    input = utility.read_input('./data/eil22_50.vrp')
     values = utility.process_input(input)
     vpr = Graph(values.get('nodes'), values.get('capacity'))
     vpr.initial_solution()
-    print(vpr.euclidean_distance(17, 20))
-    print(vpr.euclidean_distance(17, 21))
 
 
 if __name__ == '__main__':
